3a336a71f1e043dd9af991aecad0be4270af52c8
Sudhanshu Jaiswal <v-sujaiswal@expedia.com>
2020-06-02 00:38:54 +0530

    adjusted wait

--- src/chromeTest.java
+++ src/chromeTest.java
@@ -1,16 +1,17 @@
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import graphql.Assert;
+
+
 
 public class chromeTest {
 	
 	public static void main(String[]args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/sudhanshuj/Documents/drivers/chromedriver");
 		WebDriver driver=new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("http://www.github.com");
 		String title = driver.getTitle();
